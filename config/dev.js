e87ed39045300238eff3a4ccd7237ed851ae1dca
shivam123425 <shivam123425@gmail.com>
2018-08-26 23:26:36 +0530

    Intial Project

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
+  googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
+  mongoURI: "mongodb://admin:password123@ds235022.mlab.com:35022/blog_dev",
+  cookieKey: "123123123"
+};
