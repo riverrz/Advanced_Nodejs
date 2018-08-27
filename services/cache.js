283ff16e2a22a2cbe07356f2adf7db3be604c55f
shivam123425 <shivam123425@gmail.com>
2018-08-27 23:05:09 +0530

    Add .cache() on queries to allow caching it

--- services/cache.js
+++ services/cache.js
@@ -1,7 +1,40 @@
 const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require("util");
+const client = redis.createClient("redis://127.0.0.1:6379");
+
+client.get = util.promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
-mongoose.Query.prototype.exec = function() {
-  console.log("Im about to run q query");
-  return exec.apply(this, arguments);
+
+mongoose.Query.prototype.cache = function() {
+  this.useCache = true;
+  return this; // to make it chainable after .cache()
+};
+
+mongoose.Query.prototype.exec = async function() {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name
+    })
+  );
+
+  // checking if key exists in redis and return result
+  const cacheValue = await client.get(key);
+
+  if (cacheValue) {
+    const doc = JSON.parse(cacheValue);
+
+    return Array.isArray(doc)
+      ? doc.map(blog => new this.model(blog))
+      : new this.model(doc);
+  }
+  // else, issue query and store result in redis
+
+  const result = await exec.apply(this, arguments);
+  client.set(key, JSON.stringify(result));
+  return result;
 };
