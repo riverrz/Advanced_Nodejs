4add6f1eb8e89bda85285657d33d8e0439471dd5
shivam123425 <shivam123425@gmail.com>
2018-08-27 23:23:25 +0530

    Use a top level key as hash for better cache structure

--- services/cache.js
+++ services/cache.js
@@ -3,11 +3,12 @@ const redis = require("redis");
 const util = require("util");
 const client = redis.createClient("redis://127.0.0.1:6379");
 
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function() {
+mongoose.Query.prototype.cache = function(options = {}) {
+  this.hashKey = JSON.stringify(options.key || "");
   this.useCache = true;
   return this; // to make it chainable after .cache()
 };
@@ -23,7 +24,7 @@ mongoose.Query.prototype.exec = async function() {
   );
 
   // checking if key exists in redis and return result
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   if (cacheValue) {
     const doc = JSON.parse(cacheValue);
@@ -35,6 +36,6 @@ mongoose.Query.prototype.exec = async function() {
   // else, issue query and store result in redis
 
   const result = await exec.apply(this, arguments);
-  client.set(key, JSON.stringify(result));
+  client.hset(this.hashKey, key, JSON.stringify(result), "EX", 10);
   return result;
 };
