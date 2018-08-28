7ad4b25ed9a59bf58bb148b52115f49716644df7
shivam123425 <shivam123425@gmail.com>
2018-08-28 14:28:38 +0530

    Add test for checking redirect to auth flow

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,19 +1,28 @@
 const puppeteer = require("puppeteer");
 
-test("adds two numbers", () => {
-  const sum = 1 + 2;
-  expect(sum).toEqual(3);
-});
+let browser, page;
 
-test("We can launch a browser", async () => {
-  const browser = await puppeteer.launch({
+// before each test
+beforeEach(async () => {
+  jest.setTimeout(10000);
+  browser = await puppeteer.launch({
     headless: false
   });
-  const page = await browser.newPage();
+  page = await browser.newPage();
   await page.goto("localhost:3000");
+});
 
-  const text = await page.$eval("a.brand-logo", el => {
-    return el.innerHTML;
-  });
+afterEach(async () => {
+  await browser.close();
+});
+
+test("the header has correct text", async () => {
+  const text = await page.$eval("a.brand-logo", el => el.innerHTML);
   expect(text).toEqual("Blogster");
+});
+
+test("clicking login starts oauth flow", async () => {
+  await page.click(".right a");
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/);
 });
